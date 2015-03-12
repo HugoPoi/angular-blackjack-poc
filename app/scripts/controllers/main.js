7c0212caef3c51c8a6c3793e0d1b0fe264a5f239
HugoPoi <hugo.poissonnet@gmail.com>
2015-03-12 11:10:01 +0100

    Player cards implementation display

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,5 +17,15 @@ angular.module('blackjackApp')
     }
 
 
-    $scope.card1 = 0;
+    $scope.playerCards = [
+      { number: 1, state: { flipped: false } },
+      { number: 10, state: { flipped: false } },
+      { number: 12, state: { flipped: false } },
+      { number: 23, state: { flipped: false } }
+    ];
+
+
+    $scope.flip = function( card ){
+      card.state.flipped = !card.state.flipped;
+    }
   });
